dd94ab11bd4709e05fcdc25a35bebae30c9ad19d
Nathan Wong <nathan@itsallwong.com>
2025-10-13 13:07:52 -0400

    newsletter modal

---
+++ src/app/(frontend)/(pages)/newsletter/page.tsx
@@ -0,0 +1,7 @@
+import Newsletter from '../../_ui/Newsletter'
+
+const PressPage: React.FC = () => {
+  return <Newsletter />
+}
+
+export default PressPage
---
+++ src/app/(frontend)/@newsletter/(.)newsletter/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import Modal from '../../_ui/Modal'
+import Newsletter from '../../_ui/Newsletter'
+
+export default function Page() {
+  return (
+    <Modal>
+      <Newsletter />
+    </Modal>
+  )
+}
