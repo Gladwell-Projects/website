90454d1a0875e5b2c97c9bcacc02b6b9bdb8f1b2
Nathan Wong <nathan@itsallwong.com>
2025-10-13 17:55:15 -0400

    update events

--- src/app/(frontend)/(pages)/events/page.tsx
+++ src/app/(frontend)/(pages)/events/page.tsx
@@ -1,9 +1,11 @@
 import CalendarPopup from '../../_ui/CalendarPopUp'
+import Headline from '../../_ui/Headline'
 const EventsPage = () => {
   return (
-    <>
+    <div className="col-span-full grid w-full grid-cols-subgrid gap-3">
+      <Headline title="Events" className="col-span-full row-start-1 w-full" />
       <CalendarPopup hasClose={false} />
-    </>
+    </div>
   )
 }
 
