9819ce7d7ffed679c300666fb5772a071ef63076
Nathan Wong <nathan@itsallwong.com>
2025-10-29 12:47:07 -0400

    remove the ts-expect-error ? need to reboot vscode

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -64,7 +64,6 @@ export default buildConfig({
       },
     },
   },
-  //@ts-expect-error there's a typescript error that says this is not allowed. Showed up when I reinstalled the lexical editor.
   editor: lexicalDefault,
   email: resendAdapter({
     defaultFromAddress: 'noreply@notifs.gladwellprojects.com',
