8542cf277f1b5342ec7e56caf8f40979d4561443
Nathan Wong <nathan@itsallwong.com>
2025-11-05 19:25:10 -0500

    converted newsletter form to a react-hooks-form

--- src/app/(frontend)/@newsletter/(.)newsletter/page.tsx
+++ src/app/(frontend)/@newsletter/(.)newsletter/page.tsx
@@ -5,11 +5,8 @@ import Newsletter from '../../_ui/Newsletter'
 export default function Page(): React.ReactNode {
   return (
     <Modal>
-      <div className="col-span-full flex min-h-full w-full flex-col place-self-stretch">
-        <div className="grow basis-0"></div>
-        <Newsletter />
-        <div className="grow basis-0 lg:grow-2"></div>
-      </div>
+      <Newsletter />
+      <div className="col-span-full mt-8 h-4"></div>
     </Modal>
   )
 }
