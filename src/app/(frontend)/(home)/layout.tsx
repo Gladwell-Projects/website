441f29bda9c5327a153088ae052aa01f36e41504
Nathan Wong <nathan@itsallwong.com>
2026-01-13 10:31:19 -0500

    adds structured data to the home page for Site Name

--- src/app/(frontend)/(home)/layout.tsx
+++ src/app/(frontend)/(home)/layout.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import Header from '../_ui/Header'
+import { WebSite, WithContext } from 'schema-dts'
 
 const App = (props: { children: React.ReactNode }) => {
   const { children } = props
 
+  const jsonLD: WithContext<WebSite> = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: 'Gladwell Projects',
+    url: 'https://www.gladwellprojects.com',
+  }
+
   return (
     <main className="grid min-h-dvh grid-cols-12 gap-3 p-2">
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLD).replace(/</g, '\\u003c'),
+        }}
+      ></script>
       <Header navTemplate="spread" />
       {children}
     </main>
